d0775872c01dfd48943ad3e04ee3b5611ff885f4
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-06-21 16:36:54 -0400

    Empty project to start in

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    // todo: everything!
+    public static void main(String[] args) throws Exception {
+        String hello = "Hello";
+        System.out.println(hello + " World!");
+    }
+}
